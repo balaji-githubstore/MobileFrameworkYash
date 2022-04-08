73ec2c05059a79beb361e50c444ce85819767505
balaji-githubstore <balaji0017@gmail.com>
2022-04-08 12:57:28 +0530

    extent report - config

--- MobileFramework/HomeUIComponentTest.cs
+++ MobileFramework/HomeUIComponentTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AventStack.ExtentReports;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MobileFramework.Setup;
 using System;
 using System.Collections.Generic;
@@ -17,11 +18,14 @@ namespace MobileFramework
         {
             if (driver.FindElementsByXPath("//*[@text='Dismiss']").Count > 0)
             {
+                test.Log(Status.Info, "Dismiss present so click on it!!");
                 driver.FindElementByXPath("//*[@text='Dismiss']").Click();
             }
             bool homeDisplayed= driver.FindElementByXPath("//*[@text='Home']").Displayed;
             bool searchDisplayed = driver.FindElementByXPath("//*[@text='Home']").Displayed;
             bool bookmarkDisplayed = driver.FindElementByXPath("//*[@text='Bookmarks']").Displayed;
+
+            test.Log(Status.Info, "Home, Search, Bookmark displayed!!!");
 
             Assert.IsTrue(homeDisplayed,"Assertion on Home Menu Display");
             Assert.IsTrue(searchDisplayed, "Assertion on Search Menu Display");
--- MobileFramework/Setup/AppiumSetup.cs
+++ MobileFramework/Setup/AppiumSetup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
@@ -8,15 +10,41 @@ using System;
 using System.Collections.Generic;
 namespace MobileFramework.Setup
 {
+    [TestClass]
     public class AppiumSetup
     {
         private AppiumLocalService _service;
         protected AndroidDriver<IWebElement> driver;
+        public static ExtentReports extent;
+        public static ExtentTest test;
+
+        public TestContext TestContext { get; set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            //run only once at first
+            if (extent == null)
+            {
+                extent = new ExtentReports();
+                ExtentHtmlReporter reporter = new ExtentHtmlReporter(@"C:\Components\");
+                
+                extent.AttachReporter(reporter);
+            }
+        }
+
+        [AssemblyCleanup]
+        public static void End()
+        {
+            //run only once at the end
+            extent.Flush();
+        }
 
         [TestInitialize]
         public void Setup()
         {
-            
+            test= extent.CreateTest(TestContext.TestName);
+
             OptionCollector opt = new OptionCollector()
                   .AddArguments(GeneralOptionList.OverrideSession())
                   .AddArguments(new KeyValuePair<string, string>("--relaxed-security", string.Empty));
@@ -24,7 +52,7 @@ namespace MobileFramework.Setup
                 .WithLogFile(new System.IO.FileInfo("appium_log.txt")).WithArguments(opt);
             //.WithAppiumJS(new System.IO.FileInfo(@"C:\Users\JiDi\AppData\Roaming\npm\node_modules\appium\build\lib\appium.js"));
 
-            _service = builder.Build();  
+            _service = builder.Build();
             _service.Start();
 
             AppiumOptions option = new AppiumOptions();
@@ -39,9 +67,29 @@ namespace MobileFramework.Setup
         [TestCleanup]
         public void Teardown()
         {
+            if(TestContext.CurrentTestOutcome == UnitTestOutcome.Passed)
+            {
+                test.Log(Status.Pass, "Test Method Name " + TestContext.TestName + "   " + TestContext.CurrentTestOutcome + " - snapshot below");
+            }
+            else if(TestContext.CurrentTestOutcome == UnitTestOutcome.Failed)
+            {
+                test.Log(Status.Fail, "Test Method Name " + TestContext.TestName + " "+ TestContext.CurrentTestOutcome + " - snapshot below");
+            }
+            else
+            {
+                test.Log(Status.Skip, "Test Method Name " + TestContext.TestName + " " + TestContext.CurrentTestOutcome + " - snapshot below");
+            }
+
+            
+
+            Screenshot screenshot= driver.GetScreenshot();
+            test.AddScreenCaptureFromBase64String(screenshot.AsBase64EncodedString, title: TestContext.TestName);
+
             driver?.Quit();
             _service?.Dispose();
         }
+
+        
 
     }
 }
