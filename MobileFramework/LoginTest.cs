031b874a3df222ce28e575422ff28bba3b816ecf
balaji-githubstore <balaji0017@gmail.com>
2022-04-08 11:04:55 +0530

    invalid test created

--- MobileFramework/LoginTest.cs
+++ MobileFramework/LoginTest.cs
@@ -13,32 +13,42 @@ namespace MobileFramework
     [TestClass]
     public class LoginTest
     {
-        [TestMethod]
-        public void InvalidCredentialTest()
-        {
-            //start the server
+        AppiumLocalService service;
+        AndroidDriver<IWebElement> driver;
 
+        [TestInitialize]
+        public void Setup()
+        {
             OptionCollector opt = new OptionCollector()
-                .AddArguments(GeneralOptionList.OverrideSession())
-                .AddArguments(new KeyValuePair<string, string>("--relaxed-security", string.Empty));
+                  .AddArguments(GeneralOptionList.OverrideSession())
+                  .AddArguments(new KeyValuePair<string, string>("--relaxed-security", string.Empty));
             AppiumServiceBuilder builder = new AppiumServiceBuilder().UsingAnyFreePort()
                 .WithLogFile(new System.IO.FileInfo("appium_log.txt")).WithArguments(opt);
-                //.WithAppiumJS(new System.IO.FileInfo(@"C:\Users\JiDi\AppData\Roaming\npm\node_modules\appium\build\lib\appium.js"));
+            //.WithAppiumJS(new System.IO.FileInfo(@"C:\Users\JiDi\AppData\Roaming\npm\node_modules\appium\build\lib\appium.js"));
 
-            AppiumLocalService service = builder.Build(); //uses port - 4723 
+            service = builder.Build(); //uses port - 4723 
             service.Start();
-
-            Console.WriteLine(service.ServiceUrl);
-            Console.WriteLine(service.IsRunning);
 
             AppiumOptions option = new AppiumOptions();
             option.AddAdditionalCapability("platformName", "android");
             option.AddAdditionalCapability("deviceName", "bala");
             option.AddAdditionalCapability("app", @"C:\Components\khan-academy-7-3-2.apk");
 
-            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(service, option);
+            driver = new AndroidDriver<IWebElement>(service, option);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
 
+        [TestCleanup]
+        public void Teardown()
+        {
+            driver?.Quit();
+            service?.Dispose();
+        }
+
+
+        [TestMethod]
+        public void InvalidCredentialTest()
+        {
             if (driver.FindElementsByXPath("//*[@text='Dismiss']").Count > 0)
             {
                 driver.FindElementByXPath("//*[@text='Dismiss']").Click();
@@ -47,25 +57,17 @@ namespace MobileFramework
             driver.FindElementByXPath("//*[@text='Sign in']").Click();
             driver.FindElementByXPath("//*[@content-desc='Enter an e-mail address or username']").SendKeys("test@gmail.com");
             driver.FindElementByXPath("//*[contains(@text,'Pass')]").SendKeys("pass@123");
-
             if (driver.IsKeyboardShown())
             {
                 driver.HideKeyboard();
             }
-
-            //*[@text='Sign in' and @index='0']
             driver.FindElementByXPath("(//*[@text='Sign in'])[2]").Click();
-
-            String actualError = driver.FindElementByXPath("//*[contains(@text,'issue')]").Text;
-            Console.WriteLine(actualError);
+            string actualError = driver.FindElementByXPath("//*[contains(@text,'issue')]").Text;
 
             Assert.AreEqual("There was an issue signing in", actualError);
 
-            Thread.Sleep(5000);
-            driver.Quit();
-
-            //stop the server
-            service.Dispose();
         }
+
+
     }
 }
